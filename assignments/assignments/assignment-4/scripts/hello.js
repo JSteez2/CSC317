3d3d82c05f46f3b994ef8e97ade9a882fc587d77
Jaden Deldio <jadend26@icloud.com>
2025-05-18 21:19:11 -0700

    chore: move final into assignments folder

---
+++ assignments/assignments/assignment-4/scripts/hello.js
@@ -0,0 +1,7 @@
+// hello.js
+const greeting = "JavaScript";
+
+// Arrow function using a template literal
+const createMessage = (name) => `Hello, ${name}!`;
+
+console.log(createMessage(greeting));
